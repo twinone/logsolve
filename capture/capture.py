7581393dab4e8d562c0ea9e19235a9094f2dc1da
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-06-22 18:11:43 +0200

    Get only 4 hull points

--- capture/capture.py
+++ capture/capture.py
@@ -68,36 +68,36 @@ def displ():
 # Process an image
 def process(fname):
     global im
-    im = imread(fname, flatten=True)
+    # read the image from disk
+    im = imread('test/'+fname+'.jpg', flatten=True)
     add_image(fname)
 
+    # edge detection
     im = sobel(im)
-    #add_image('Sobel')
-
-#    im = im > .03
+    # blurring
     im = filters.gaussian(im, sigma=1)
-
+    # thresholding: convert to binary image
     loc = threshold_local(im, 57)
     im = im > loc
     add_image('Threshold')
 
+    # detect straight lines longer than 150px
     segs = probabilistic_hough_line(
         im,
         threshold=10,
         line_length=150,
         line_gap=5)
 
-    im[:] = 0
+    # draw the segments
+    im[:] = 0 # set image to black
     for seg in segs:
         ((x1, y1), (x2, y2)) = seg
-        #print("line from", x1,y1, 'to', x2, y2)
         rr,cc = draw.line(y1,x1,y2,x2)
         im[rr, cc] = 1
-
     add_image('Hough Lines')
 
-    print("Iterating over",len(segs), "segments")
-    ccs = []  # Connected components
+    # Calculate the connected components (intersecting groups)
+    ccs = []
     while segs:
         s = segs.pop()
         found = False
@@ -108,21 +108,20 @@ def process(fname):
                     found = True
                     break
             if found: break
-
         if not found: ccs.append([s])
 
+    # get the one with most segments (the one with the grid)
     ccs.sort(key=len)
     ccs = ccs[::-1]
-    print("Found", len(ccs), "connected components")
-    if (len(ccs) >= 1):
-        print("Longest cc:", len(ccs[0]))
 
+    # get coordinates for each segment
     coords = []
     for seg in ccs[0]:
         (x1, y1), (x2,y2) = seg
         coords.append([x1, y1])
         coords.append([x2, y2])
 
+    # calculate the convex hull or 'envelope' surrounding segments
     hull = ConvexHull(np.array(coords))
     indices = hull.vertices
     im[:] = 0
@@ -130,13 +129,37 @@ def process(fname):
         x, y = coords[i]
         rr, cc = draw.circle(y,x,1)
         im[rr, cc] = 1
-    print("Number of hull indices:", len(indices))
     im = morphology.convex_hull_image(im)
-    for i in indices:
-        x, y = coords[i]
+    print(fname + ":", "number of hull vertices:", len(indices))
+
+    hullcoords = [coords[i] for i in indices]
+    # calculate the 4 vertices of the trapezoid
+    # these are the vertices that are most top-left, top-right,
+    # bottom-left and bottom-right
+    # we can easily calculate them maximizing (+,-)(x,y)
+    # for example: the top left corner will be the vertex
+    # whose -x-y is maximum
+    def findmax(fx, fy):
+        maximum = None
+        res = None
+        for (x,y) in hullcoords:
+            if maximum == None or x*fx + y*fy > maximum:
+                maximum = x*fx+y*fy
+                res = x, y
+        return res
+    verts = [
+        findmax(-1,-1),
+        findmax(-1, 1),
+        findmax( 1,-1),
+        findmax( 1, 1),
+    ]
+
+    # display the actual vertices a bit bigger
+    for (x, y) in verts:
         rr, cc = draw.circle(y,x,10)
         im[rr, cc] = 1
     add_image('Hull')
+
 
 
 
@@ -156,10 +179,11 @@ def intersect(s1,s2):
 
 # Main
 def main():
-    #for i in range(0, 1):
-    #    process('test'+str(i)+'.jpg')
-    process('test/test0.jpg')
-    #process('test/test1.jpg')
+    #names = ['pearl', 'range', 'tents', 'unruly']
+    names = ['fifteen', 'galaxies', 'keen', 'loopy']
+    for name in names:
+        process(name+'0')
+    #process('test/1.jpg')
     #process('test/test3.jpg')
     #process('test/test6.jpg')
 
