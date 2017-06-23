784656499ac6d1333d438aa45ec96a155e052cb8
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-06-23 11:42:25 +0200

    Add tester for gridsize

---
+++ solve/test_gridsize.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import sys
+from gridsize import grid_size
+
+def usage():
+    print("Usage:", sys.argv[0], '<input-image> <x> <y>')
+    exit(1)
+# Main
+def main():
+    if len(sys.argv) < 4:
+        usage()
+
+    fname, real_x, real_y = sys.argv[1:4]
+    try:
+        real = int(real_x), int(real_y)
+    except:
+        usage()
+
+    detected = grid_size(fname)
+    print(real, detected)
+    if real != detected:
+        print('[FAIL]', fname+': expected', coords_str(real) + ', got', coords_str(detected))
+        exit(1)
+    else:
+        print('[PASS]', fname+':', coords_str(detected))
+
+
+def coords_str(c):
+    x, y = c
+    return str(x) + 'x' + str(y)
+
+
+
+if __name__ == '__main__':
+    main()
