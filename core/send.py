fe72342c5b57d30f4f236d8b03586516c2f9c411
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-07-23 19:05:46 +0200

    Update to use new calibration format

--- core/send.py
+++ core/send.py
@@ -2,13 +2,15 @@ import sys
 
 from comm import PrintComm
 
-# left, right, top, bottom, center_y, up
-calib = (57, 146, 177, 22, 103, 13)
+# left, right, cy, up
+calib = (65, 144, 110, 16)
+
+CLICK_DST = 4
 
 
 # tested on a 8x8 grid
 # percentage of the screen the game actually occupies
-PCT_W = 0.9
+PCT_W = 1
 
 INV_X = 1
 INV_Y = -1
@@ -23,12 +25,11 @@ class Sender:
         self.gs = gs
 
         self.width = abs(self.calib[1] - self.calib[0])
-        self.height = abs(self.calib[3] - self.calib[2])
 
         self.cx = self.calib[0] + self.width / 2
-        self.cy = self.calib[4]
+        self.cy = self.calib[2]
 
-        self.z_dst = self.calib[5]
+        self.up = self.calib[3]
 
         cols, rows = self.gs
         self.cellsize = self.width * PCT_W / cols
@@ -38,13 +39,48 @@ class Sender:
 
     # goto (row,col) (starting at 0)
     def goto(self, row, col):
-        x = self.left + ((col+0.5)*self.cellsize)*INV_X
-        y = self.top + ((row+0.5)*self.cellsize)*INV_Y
+
+        x = self.pc.x if not col else self.left + ((col+0.5)*self.cellsize)*INV_X
+        y = self.pc.y if not row else self.top + ((row+0.5)*self.cellsize)*INV_Y
         self.pc.set_xy(x, y)
 
 
     def click(self):
-        self.pc.click(self.z_dst)
+        self.pc.click(CLICK_DST)
+
+    def center(self):
+        self.pc.set_xy(self.cx, self.cy)
+
+
+    def cli(self):
+        while True:
+            try:
+                line = input('> ')
+            except EOFError:
+                break
+
+            split = line.split(' ')
+            cmd = split[0]
+            if (cmd == 'click'):
+                self.click()
+            if (cmd == 'dclick'):
+                self.click()
+                self.click()
+            if (cmd == 'row'):
+                row = int(split[1])
+                self.goto(row, None)
+            if (cmd == 'col'):
+                col = int(split[1])
+                self.goto(None, col)
+            if (cmd == 'goto'):
+                row = int(split[1])
+                col = int(split[2])
+                self.goto(row, col)
+            if (cmd == ''):
+                self.click()
+            else:
+                self.pc.write(line)
+
 
 
 
@@ -60,12 +96,13 @@ def main():
     print('Home all (G28)')
     pc.write('G28')
 
-    pc.offset_z(20)
+    pc.offset_z(calib[3]+CLICK_DST)
 
     s = Sender(pc, calib, (8,8))
 
-    s.goto(1,3)
-    s.click()
+    s.center()
+    s.cli()
+
 
 
 
