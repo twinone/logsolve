822fbe9437fc90696b8d6fea43af721c8cfbbb77
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-07-23 19:07:28 +0200

    Add from_arg.py

---
+++ core/from_arg.py
@@ -0,0 +1,15 @@
+import sys
+
+from skimage.io import imread, imsave
+from .undistort import undistort
+
+
+def from_arg():
+    if len(sys.argv) < 2:
+        print("Usage:", sys.argv[0], '<input-image>')
+        exit(1)
+
+    infile = sys.argv[1]
+    im = imread(infile, flatten=True)
+
+    return im
