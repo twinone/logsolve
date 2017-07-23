0657c2a5fd1fb806f96d7cc390265edcd5364b23
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-07-23 19:04:58 +0200

    Modify calibration process

--- calibrate.py
+++ calibrate.py
@@ -32,41 +32,40 @@ def main():
     print('Home all (G28)')
     pc.write('G28')
 
-    print('Centering (20mm up for safety)')
-    pc.offset_z(20)
+    print('Centering (50mm up for safety)')
+    pc.offset_z(50)
 
     x, y = BED_SIZE
     bed_cx, bed_cy = x/2, y/2
     pc.set_xy(bed_cx, bed_cy)
 
     print('Align the touch tip to the current edge.')
-    print('Enter mm to move in x axis, 0 to end')
-    print('Calibrating LEFT edge')
-    calib(lambda x: pc.offset_xy(x, 0))
-    left = pc.x
-    print('Calibrating RIGHT edge')
-    calib(lambda x: pc.offset_xy(x, 0))
-    right = pc.x
+    print('Enter mm to move in axis, 0 to end')
 
-    # go to the center of the x axis
-    cx = int((left+right)/2)
-    pc.set_xy(cx, pc.y)
+    print('Move Z down until it touches the screen')
+    calib(lambda x: pc.offset_z(x))
+    up = pc.z
 
-    print('Calibrating TOP edge')
-    calib(lambda x: pc.offset_xy(0, x))
-    top = pc.y
-    print('Calibrating BOTTOM edge')
-    calib(lambda x: pc.offset_xy(0, x))
-    bottom = pc.y
+    pc.offset_z(5)
 
-    print('Now center vertically on the game grid')
+    print('Center vertically on the game grid')
     calib(lambda x: pc.offset_xy(0, x))
     game_cy = pc.y
 
+    print('Center on the LEFT edge of the grid')
+    calib(lambda x: pc.offset_xy(x, 0))
+    left = pc.x
 
-    print('Calibrated:', (left, right, top, bottom, game_cy))
-    cy = int((top+bottom)/2)
-    pc.set_xy(cx, cy)
+    print('Center on the RIGHT edge of the grid')
+    calib(lambda x: pc.offset_xy(x, 0))
+    right = pc.x
+
+
+
+
+    print('Calibrated:', (left, right, game_cy, up))
+    game_cx = (left+right)/2
+    pc.set_xy(game_cx, game_cy)
 
 
 
