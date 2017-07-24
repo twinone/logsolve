26461b3fd476a1d0c064698b0ec71f3aa1d442ae
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-07-25 01:23:10 +0200

    Integrate everything (listen, process, solve)

--- games/unruly/capture.py
+++ games/unruly/capture.py
@@ -1,16 +1,42 @@
 import logsolve as ls
 
-def main():
+from subprocess import Popen, PIPE, STDOUT
+
+
+
+OUT_FILE = './image.jpg'
+
+def process(fname):
+    ls.log('Reading image')
+    im = ls.imread(fname, flatten=True)
     ls.log('Undistorting')
-    im = ls.undistort_from_arg()
+    im = ls.undistort(im)
     ls.log('Calculating grid size...')
     gs = ls.gridsize(im)
     ls.log('Grid size: ', gs)
     ls.log('Calculating cell colors')
-    colors = ls.thresh_local_mean(im, gs)
-    out = ls.printmat(colors)
-    print(out)
+    colors = ls.thresh_local_mean(im, gs, debug=True)
+    mat = ls.printmat(colors)
+    ls.log("Problem matrix:")
+    ls.log(mat)
 
+    ls.log("Calculating solution...")
+
+    p = Popen(['./solve'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    sol = p.communicate(input=mat.encode())[0]
+    ls.log("Solution:")
+    ls.log(sol.decode())
+
+
+def serve():
+    srv = ls.Server(OUT_FILE)
+    srv.set_handlerfunc(process)
+    srv.run('0.0.0.0')
+
+
+def main():
+    #serve()
+    process('./image.jpg')
 
 if __name__ == '__main__':
     main()
